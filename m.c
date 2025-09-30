ef7507cba2a8f0aff6ca08c2c0a00ab2ea91b73c
trs-1342 <socialmediaoftrs1342@gmail.com>
2025-09-30 16:45:59 +0300

    update

--- m.c
+++ m.c
@@ -13,20 +13,16 @@ int mat()
     printf("\n 1) +\n 2) -\n 3) *\n 4) /\nislemi sec: ");
     scanf("%f", &i);
 
-    if (i == 1)
-    { // TOPLAMA +
+    if (i == 1) { // TOPLAMA +
         printf("+: %f", s1 + s2);
     }
-    else if (i == 2)
-    { // CIKARMA -
+    else if (i == 2) { // CIKARMA -
         printf("-: %f", s1 - s2);
     }
-    else if (i == 3)
-    { // CARPMA *
+    else if (i == 3) { // CARPMA *
         printf("*: %f", s1 * s2);
     }
-    else if (i == 4)
-    { // BOLME /
+    else if (i == 4) { // BOLME /
         printf("/: %f", s1 / s2);
     }
     else
