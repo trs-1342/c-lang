c8a4eb958b8b71dfe6e4daca7a967428a152ac30
trs-1342 <socialmediaoftrs1342@gmail.com>
2025-10-02 22:25:48 +0300

    update ders-2/main.c (?) and add ders-3/main.c file.

---
+++ ders-3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+	char* ad;
+
+	printf("ad giriniz: ");
+	scanf("%s", ad); // girilen degeri ad(belirtilen) degiskene atama
+
+	printf("girilen ad: %s", ad);
+
+	return 0;
+}
