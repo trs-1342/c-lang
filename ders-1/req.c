3a2d4befb1086f10254323d8ee132f17e142a86f
trs-1342 <socialmediaoftrs1342@gmail.com>
2025-10-02 21:21:57 +0300

    add ders-1/req.c file.

---
+++ ders-1/req.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+{
+
+	printf("Ogrenci 1\n");
+	printf("Ad: Halil\n");
+	printf("Soyad: Hattab\n");
+	printf("Sinif: 11\n");
+	printf("Sube: E\n\n");
+	printf("==============\n\n");
+	printf("Ogrenci 2\n");
+	printf("Ad: Ozan\n");
+	printf("Soyad: Aksoy\n");
+	printf("Sinif: 11\n");
+	printf("Sube: D\n");
+	return 0;
+}
