1a7c53323e5fa2bf9bc487291a31c5e1f04d7bf8
trs-1342 <socialmediaoftrs1342@gmail.com>
2025-10-02 22:05:37 +0300

    update ders-2/main.c file.

--- ders-2/main.c
+++ ders-2/main.c
@@ -3,7 +3,7 @@
 
 int main() 
 {
-	char arabaMarkasi[10]="Tesla";
+	/*char arabaMarkasi[10]="Tesla";
 	char arabaSahibi[15]="Elon Musk";
 
 	printf("%s\n", arabaMarkasi);
@@ -13,17 +13,29 @@ int main()
 
 	char arabaDegeri[20]="1.3 Trilyon Dolar";
 
-	printf("Araba Degeri: %s\n", arabaDegeri);
+	printf("Araba Degeri: %s\n", arabaDegeri);*/
 
-	// ===
+	// === === === === === ===
 
-	char ogrenciAdi[10]="Halil";
-	char ogrenciSoyadi[13]="Hattab Hamso";
-	char ogrenciIli[10]="Istanbul";
-	char ogrenciYasi[2]="18";
-	char ogrenciDogumYili[4]="2007";
-	char ogrenciDogumAyi[10]="Ocak";
-	char ogrenciDogumGunu[2]="01";
+	// char kullanirken bu sorunu cikartabilir:
+	// C’de string sonlandırma ('\0') taşması, yani ekranda degerlerin tasimasi bozuk olarak cikti verilmesi.
+	// cozum: 'degisken[]' yani dizi icinde bos birakip kendisi otomatik olarak tamamlar.
+
+	// char ogrenciAdi[10]="Halil";
+	// char ogrenciSoyadi[13]="Hattab Hamso";
+	// char ogrenciIli[10]="Istanbul";
+	// char ogrenciYasi[2]="18";
+	// char ogrenciDogumYili[4]="2007";
+	// char ogrenciDogumAyi[10]="Ocak";
+	// char ogrenciDogumGunu[2]="01";
+
+	char ogrenciAdi[]="Halil";
+	char ogrenciSoyadi[]="Hattab Hamso";
+	char ogrenciIli[]="Istanbul";
+	char ogrenciYasi[]="18";
+	char ogrenciDogumYili[]="2007";
+	char ogrenciDogumAyi[]="Ocak";
+	char ogrenciDogumGunu[]="01";
 
 	printf("\n\n****** Kisi Tanitimi ******\n");
 
